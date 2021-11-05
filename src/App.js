bde6554b1bafe2ad0642e8df6ef1bffa54699a88
MLMeyer <meyer.marie.louise@gmail.com>
2021-11-05 16:56:35 +0100

    Created components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
+import Footer from './components/footer/Footer';
+import Nav from './components/nav/Nav';
 
 function App() {
   return (
     <div className="App">
-      
+      <Footer />
+      <Nav />
     </div>
   );
 }
