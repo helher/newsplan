faffcb412af05e1dd97ec5583f5fea9c65d9076c
Helena Hermansen <herm@itu.dk>
2021-11-09 22:08:55 +0100

    Structure of Components
    This commit includes the initial structure of components. More specifically, the components created are:
     - IconButton
     - TextButton
     - CardContainer
     - Card
     - DropdownCalendar
     - DropdownText
     - ArticleListView
     - IdeaListView
     - SearchBar
     - Tag

---
+++ src/components/dropdowns/DropdownText/DropdownText.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DropdownText extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default DropdownText;
---
+++ src/components/list-views/article-list-view/ArticleListView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ArticleListView extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ArticleListView;
