a016775223c0d84a1331f22357ee893e593145d3
Helena Hermansen <herm@itu.dk>
2021-11-12 16:35:24 +0100

    Fixed Navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+// Screens
+import TeamPlan from './screens/TeamPlan'
+import Dashboard from './screens/Dashboard';
+import Ideas from './screens/Ideas';
+import Articles from './screens/Articles';
 
 // Components
 import NavigationBar from './components/nav/NavigationBar'
 
 function App() {
   return (
-    
-
-        <NavigationBar/>
-
-    
+    <Router>
+      <div class="app">
+      <NavigationBar/>
+        <Routes>
+          <Route path="/" element={ <Dashboard /> } exact/>
+          <Route path="/articles" element={ <Articles /> } exact/>
+          <Route path="/ideas" element={ <Ideas /> } exact/>
+          <Route path="/teamplan" element={ <TeamPlan /> } exact/>
+        </Routes>
+    </div>
+  </Router>
   );
 }
 
--- src/components/nav/NavigationBar.js
+++ src/components/nav/NavigationBar.js
@@ -1,42 +1,28 @@
-import React from 'react'
-import './NavigationBar.css'
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
-
-// Screens
-import Dashboard from '../../screens/Dashboard'
-import Articles from '../../screens/Articles'
-import Ideas from '../../screens/Ideas'
-import TeamPlan from '../../screens/TeamPlan'
+import React from 'react';
+import './NavigationBar.css';
+import {Link} from 'react-router-dom';
 
 function NavigationBar() {
     return (
-        <Router>
-            <div>
-                <nav className="nav">
-                    <ul>
-                        <li>
-                            <Link to="/">Dashboard</Link>
-                        </li>
-                        <li>
-                            <Link to="/articles">Articles</Link>
-                        </li>
-                        <li>
-                            <Link to="/ideas">Ideas</Link>
-                        </li>
-                        <li>
-                            <Link to="/teamplan">Team Plan</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Routes>
-                    <Route path="/" element={ <Dashboard /> } exact/>
-                    <Route path="/articles" element={ <Articles /> } exact/>
-                    <Route path="/ideas" element={ <Ideas /> } exact/>
-                    <Route path="/teamplan" element={ <TeamPlan /> } exact/>
-                </Routes>
-            </div>
-        </Router>
-    )
-}
+        <div>
+            <nav className="nav">
+                <ul>
+                    <li>
+                        <Link to="/">Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link to="/articles">Articles</Link>
+                    </li>
+                    <li>
+                        <Link to="/ideas">Ideas</Link>
+                    </li>
+                    <li>
+                        <Link to="/teamplan">Team Plan</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
 
-export default NavigationBar
+export default NavigationBar;
