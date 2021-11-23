4e8f0c48c80d5327642543d927d7ec472edf0508
Anitamalina <anit@itu.dk>
2021-11-23 14:00:04 +0100

    calender working - still need some prety

--- src/components/dropdowns/DropdownCalendar/DropdownCalendar.js
+++ src/components/dropdowns/DropdownCalendar/DropdownCalendar.js
@@ -1,23 +1,43 @@
 import React, { useState } from 'react';
 import './DropdownCalendar.css';
-import DatePicker from 'react-datepicker';
-import "react-datepicker/dist/react-datepicker.css";
+import { IoMdArrowDropdownCircle } from 'react-icons/io'
+
+// Calender
+import DatePicker from 'react-modern-calendar-datepicker';
+// import 'react-modern-calendar-datepicker/lib/DatePicker.css';
 
 
 
 const DropdownCalendar = () => {
-  const [startDate, setStartDate] = useState(new Date());
+    const [selectedDay, setSelectedDay] = useState(null);
+
+
+    const formatInputValue = () => {
+      if (!selectedDay) return '';
+
+      let month = `${selectedDay.month}`
+      const months = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+      let newMonth = months[month]
+
+      return `${newMonth} ${selectedDay.day}, ${selectedDay.year}`;
+    }
   
-  return (
-    <div className="dropdown">
+    return (
+        <div>
         <h5>Expiration Date</h5>
-        <div className="dropdown-select">
-            <DatePicker className="datePicker" selected={startDate} onChange={(date) => setStartDate(date)} />
-        </div>
-    </div>
-    
-        
-  )
-}
+        <DatePicker
+            value={selectedDay}
+            onChange={setSelectedDay}
+            inputPlaceholder="Select" // placeholder
+            formatInputText={formatInputValue} // format value
+            inputClassName="datePicker" // custom class
+            shouldHighlightWeekends
+        />
+            <IoMdArrowDropdownCircle className="dropdowncalender-icon"/>
+
+      </div>
+    );
+  };
 
 export default DropdownCalendar;
