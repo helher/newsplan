5aa0994827f93ab4ed5cbba576c02df130e3a33c
Anitamalina <anit@itu.dk>
2021-11-28 08:23:34 +0100

    color prop

--- src/components/statusbar/Statusbar.js
+++ src/components/statusbar/Statusbar.js
@@ -2,9 +2,16 @@ import React from 'react'
 import './Statusbar.css';
 
 function Statusbar() {
+    
+
+    function Color() {
+
+    }
+
+
     return(
         <div className = "bar">
-            
+
         </div>
     )
 }
