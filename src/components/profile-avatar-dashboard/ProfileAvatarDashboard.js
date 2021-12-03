701c804c2319dd33952ce6a6821dce825a38f12f
Aliz Szakacs <alsz@itu.dk>
2021-12-03 11:36:09 +0100

    avatar-setup

---
+++ src/components/profile-avatar-dashboard/ProfileAvatarDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ProfileAvatarDashboard.css'
+
+
+function ProfileAvatarDashboard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProfileAvatarDashboard
