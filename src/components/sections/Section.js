8f07e86f5f67be4b3b4e903e59364ea008badef7
Anitamalina <anit@itu.dk>
2021-12-05 17:37:00 +0100

    Sections start

--- src/components/sections/Section.js
+++ src/components/sections/Section.js
@@ -7,6 +7,21 @@ const Section = ({
         color
     }) => {
 
+
+    async function readSections () {
+        // Reading parse objects is done by using Parse.Query
+        const parseQuery = new Parse.Query('Section')
+        try {
+            let sections = await parseQuery.find()
+            console.log(sections)
+            return true
+        } catch (error) {
+            // Error can be caused by lack of Internet connection
+            alert(`Error! ${error.message}`)
+            return false;
+        }
+    }
+
     function Colors(props) {
         return (
             <div className="colors" style={{backgroundColor: props.color}}>{props.children}</div>
