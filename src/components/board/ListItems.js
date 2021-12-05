7436070c9dd4beb6706bb621a8acab9e8d0d014d
Helena Hermansen <herm@itu.dk>
2021-12-05 18:33:34 +0100

    Established: Connection to Database

--- src/components/board/ListItems.js
+++ src/components/board/ListItems.js
@@ -2,11 +2,31 @@
 import { Draggable } from "react-beautiful-dnd";
 import { LoremIpsum } from "lorem-ipsum";
 import React, { useMemo } from "react";
+import Parse from "parse";
 
 // Styling
 import './Board.css';
 
 const lorem = new LoremIpsum();
+
+
+
+read();
+
+function read() {
+  const RetrieveBoardData = new Parse.Query('Idea');
+  RetrieveBoardData.equalTo('title');
+  RetrieveBoardData.first().then(function(idea){
+    if(idea){
+      console.log('Idea found successfully with the title: ' + idea.get("title") + ' ' + 'and was updated at ' + idea.get('updatedAt'));
+    } else {
+        console.log("Nothing found, please try again");
+    }
+  }).catch(function(error){
+    console.log("Error: " + error.code + " " + error.message);       
+  });
+}
+
 
 const ListItem = ({ item, index }) => {
   const randomHeader = useMemo(() => lorem.generateWords(5), []);
