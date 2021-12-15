df53c7399924dba883b1ce447177cbac1b10fd25
Anitamalina <anit@itu.dk>
2021-12-15 13:51:35 +0100

    nyt commit

--- src/screens/Ideas.js
+++ src/screens/Ideas.js
@@ -7,6 +7,9 @@ import PopupIdea from '../components/popups/popup-idea/PopupIdea';
 import AddIdeaButton from '../components/buttons/AddIdeaButton/AddIdeaButton';
 import LoadButton from '../components/buttons/LoadButton/LoadButton';
 import { DataGrid } from '@mui/x-data-grid';
+import { List } from "antd";
+import { easing } from '@mui/material';
+
 
 const columns = [
     {
@@ -43,71 +46,93 @@ function Ideas() {
 
     // useEffects runs only once, first time the componenten renders, because we have an empty dependency!
     useEffect(() => {
-        console.log("useffect ran")
-        getIdeas().then(idea => setIdeaTable(idea));
+        fetch()
+        .then((idea) => idea.json())
+        .then((idea) => setIdeaTable(idea))
       }, []);
 
 
-    function getIdeas () {
-        let Ideas = []
-        const ideas = new Parse.Query('Idea');
-        
-        return ideas.find().then((results) => {
-            Ideas = results.map((idea) => {
+    
+    async function readIdeas() {
+      const query = new Parse.Query("Idea");
+        try {
+          const ideas = await query.find();
+          setIdeaTable(ideas);
+          return true;
+        } catch(error){
+           alert(`Error ${error.message}`);
+           return false;
+        } 
+      }
+
+/*      function destructureIdeas () {
+        ideaTable.then(results.map((idea) => {
               return {
                 id: idea.objectId,
-                title: idea.title,
+                title: idea.get("title"),
                 section: idea.tags,
                 author: idea.user,
                 expiration: idea.expiration
               };
-            });
+            }
             return Ideas;
   
         }, (err) => {
           console.log(err);
         });
-    }
-
-
-      
-      const rows = [
-        { id: 1, section: 'Snow', title: 'Jon', author: 35 },
-        { id: 2, section: 'Lannister', title: 'Cersei', author: 42 },
-        { id: 3, section: 'Lannister', title: 'Jaime', author: 45 },
-        { id: 4, section: 'Stark', title: 'Arya', author: 16 },
-        { id: 5, section: 'Targaryen', title: 'Daenerys', author: null },
-        { id: 6, section: 'Melisandre', title: null, author: 150 },
-        { id: 7, section: 'Clifford', title: 'Ferrara', author: 44 },
-        { id: 8, section: 'Frances', title: 'Rossini', author: 36 },
-        { id: 9, section: 'Roxie', title: 'Harvey', author: 65 },
-      ];
+    } */
 
 
 
     return (
-        <>
-            <div className="idea">
+      <>
+         <div className="idea">
                 <div className="idea-table" />
                     <div style={{ height: 400, width: '100%' }}>
                         <DataGrid
-                            rows={rows}
+                            rows={ideaTable}
                             columns={columns}
                             pageSize={5}
                             rowsPerPageOptions={[5]} />
                     </div>
-            </div>
-            <PopupIdea popup={popup} setPopup={setPopup} ideaId={ideaId} />
-    
-            <div className="footer-container">
-                <div className="footeridea-btns">
-                    <AddIdeaButton text="Add Idea" popup={popup} setPopup={setPopup} setIdeaId={setIdeaId} />
-                    <LoadButton text="Load more Ideas" />
-                </div>
-                <Footer />
-            </div>
-        </>
-    )
+            </div> 
+
+        <div>
+          {ideaTable !== null &&
+            ideaTable !== undefined &&
+            ideaTable.length > 0 && (
+              <List
+                dataSource={ideaTable}
+                renderItem={(idea) => (
+                  <List.Item >
+                    <div>
+                          <p className="comment-content">
+                            {idea.get("title")}
+                          </p>
+                      
+                    </div>
+                  </List.Item>
+                )}
+              />
+            )}
+        </div>
+                 
+        <PopupIdea popup={popup} setPopup={setPopup} ideaId={ideaId} />
+
+        <div className="footer-container">
+          <div className="footeridea-btns">
+            <AddIdeaButton
+              text="Add Idea"
+              popup={popup}
+              setPopup={setPopup}
+              setIdeaId={setIdeaId}
+            />
+            <LoadButton text="Load more Ideas" />
+          </div>
+          <Footer />
+        </div>
+      </>
+    );
 }
 
 export default Ideas;
