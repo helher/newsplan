94c9f077f27df1f863baaa0bb6732ac1eee7ba09
MLMeyer <meyer.marie.louise@gmail.com>
2021-12-15 16:06:08 +0100

    Updated workload dropdown

--- src/components/dropdowns/AssignWorkload/AssignWorkload.js
+++ src/components/dropdowns/AssignWorkload/AssignWorkload.js
@@ -1,42 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import './AssignWorkload.css';
-import { IoMdArrowDropdownCircle } from 'react-icons/io'
-import Parse from 'parse';
+import React, { useState, useEffect } from "react";
+import "./AssignWorkload.css";
+import { IoMdArrowDropdownCircle } from "react-icons/io";
+import Parse from "parse";
+import { List } from "antd";
 
+function AssignWorkload() {
+  const [isActive, setIsActive] = useState(false);
+  const [readResults, setReadResults] = useState([]);
 
-function AssignWorkload({selectedWorkload, setSelectedWorkload}) {
+  const readWorkload = async function () {
+    let query = new Parse.Query("Workload");
+    try {
+      let employees = await query.find();
 
-    const [isActive, setIsActive] = useState(false)
-    const options = ["all night", "all day", "chill amount", "unknown"];
+      setReadResults(employees);
+      return true;
+    } catch (error) {
+      alert(`Error! ${error.message}`);
+      return false;
+    }
+  };
 
-
-    return (
-        <div className = "workload-dropdown">
-            <br></br>
-            <br></br>
-            <h5>Workload</h5>
-            <div className="dropdown-btn" onClick={() => setIsActive(!isActive)}>
-                    {!selectedWorkload && (<p>Select</p>)}
-                    <div className="selected-employee">{selectedWorkload}</div>
-                    <div className="space"></div>
-                    <IoMdArrowDropdownCircle className="dropdown-icon"/>
-                </div>
-                {isActive && (
-                    <div className="dropdown-content">
-                        {options.map((option) => ( 
-                            <div
-                                onClick={(e) => {
-                                    setSelectedWorkload(option)
-                                    setIsActive(false)
-                                }}
-                                className="dropdown-item">
-                                    <h6>{option}</h6>
-                            </div>
-                        ))}
-                    </div>
-                )}
-        </div>
-    )
+  return (
+    <div className="workload-dropdown" onClick={readWorkload}>
+      <h5>Employee</h5>
+      <div className="dropdown-btn" onClick={() => setIsActive(!isActive)}>
+         <p>Select</p>
+        <div className="space"></div>
+        <IoMdArrowDropdownCircle className="dropdown-icon" />
+      </div>
+      <div className="dropdown-content">
+        {isActive &&
+          readResults !== null &&
+          readResults !== undefined &&
+          readResults.length > 0 && (
+            <List
+              dataSource={readResults}
+              renderItem={(item) => (
+                <List.Item>
+                  <div
+                    onClick={(e) => {
+                      setIsActive(false);
+                    }}
+                    className="dropdown-item"
+                  >
+                    <h6>
+                      {item.get("duration")}
+                    </h6>
+                  </div>
+                </List.Item>
+              )}
+            />
+          )}
+      </div>
+    </div>
+  );
 }
 
-export default AssignWorkload
+export default AssignWorkload;
