11b72843ff67943cf00ef2363f3166ad640b4d1b
Aliz Szakacs <alsz@itu.dk>
2021-12-21 10:46:14 +0100

    Visibility_parsing_from_DB

--- src/components/dropdowns/DropdownVisibility/DropdownVisibility.js
+++ src/components/dropdowns/DropdownVisibility/DropdownVisibility.js
@@ -1,39 +1,73 @@
-import React, { useState } from 'react';
-import './DropdownVisibility.css';
-import { IoMdArrowDropdownCircle } from 'react-icons/io'
+import React, { useState, useEffect } from "react";
+import "./DropdownVisibility.css";
+import { IoMdArrowDropdownCircle } from "react-icons/io";
+import Parse from 'parse'; 
 
 
-function DropdownVisibility ({visibility, setVisibility}) {
-    const [isActive, setIsActive] = useState(false)
-    const options = ['Me Only', 'Editors and myself', 'Everyone']
+function DropdownVisibility({ visibility, setVisibility }) {
+  const [isActive, setIsActive] = useState(false);
+  const [readResults, setReadResults] = useState([]);
+  let visibilityOptions = []; 
 
-    return (
-        <div className="dropdown">
-            <h5>Visibility</h5>
-            <div className="dropdown-select">
-                <div className="dropdown-btn" onClick={() => setIsActive(!isActive)}>
-                    {!visibility && (<p>Select</p>)}
-                    <div className="selected-visibility">{visibility}</div>
-                    <div className="space"></div>
-                    <IoMdArrowDropdownCircle className="dropdown-icon"/>
-                </div>
-                {isActive && (
+
+  useEffect(() => {
+    readWorkload();
+  }, []);
+
+  useEffect(() => {
+  }, [readResults]);
+
+  const readWorkload = async function () {
+    let query = new Parse.Query("Visibility");
+    let visibilityOption = await query.find(); 
+
+    try {
+        visibilityOption.forEach(visibilityOption => {
+        visibilityOptions.push(visibilityOption.get("name"));
+        })
+
+      setReadResults (visibilityOptions) ;
+      return true;
+
+    } catch (error) {
+      alert(`Error! ${error.message}`);
+      return false;
+    }
+  };
+
+  
+
+  return (
+    <div className="dropdown" onClick = {readWorkload}>
+      <h5>Visibility</h5>
+      <div className="dropdown-select">
+        <div className="dropdown-btn" onClick={() => setIsActive(!isActive)}>
+          {!visibility && <p>Select</p>}
+          <div className="selected-visibility">{visibility}</div>
+          <div className="space"></div>
+          <IoMdArrowDropdownCircle className="dropdown-icon" />
+
+        </div>
+      <div className="dropdown-content">
+      {isActive && (
                     <div className="dropdown-content">
-                        {options.map((option) => ( 
+                        {readResults.map((visibilityOption) => ( 
                             <div 
                                 onClick={(e) => {
-                                    setVisibility(option)
+                                  console.log("is the fail happening here?")
+                                    setVisibility(visibilityOption)
                                     setIsActive(false)
                                 }}
                                 className="dropdown-item">
-                                    <h6>{option}</h6>
+                                    <h6>{visibilityOption}</h6>
                             </div>
                         ))}
                     </div>
                 )}
-            </div>
-        </div>
-    )
+      </div>
+    </div>
+      </div>
+  );
 }
 
 export default DropdownVisibility;
