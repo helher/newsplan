71284d8a105a01e203b0ba013fed9a0ce5998fbf
Anitamalina <anit@itu.dk>
2021-12-30 14:42:06 +0100

    cleaned dropdown

--- src/components/dropdowns/dropdown-calendar/DropdownCalendar.js
+++ src/components/dropdowns/dropdown-calendar/DropdownCalendar.js
@@ -5,9 +5,6 @@ import './DropdownCalendar.css';
 
 // Calender
 import DatePicker from 'react-modern-calendar-datepicker';
-// import 'react-modern-calendar-datepicker/lib/DatePicker.css';
-
-
 
 const DropdownCalendar = ({date, setDate}) => {
 
@@ -27,11 +24,10 @@ const DropdownCalendar = ({date, setDate}) => {
         <DatePicker
             value={date}
             onChange={setDate}
-            inputPlaceholder="Select" // placeholder
-            formatInputText={formatInputValue} // format value
-            inputClassName="datePicker" // custom class
+            inputPlaceholder="Select"
+            formatInputText={formatInputValue}
+            inputClassName="datePicker"
         />
-            {/* <IoMdArrowDropdownCircle className="dropdowncalender-icon"/> */}
             </>
     );
   };
