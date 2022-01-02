c9a9b5f75c5d808a78824ec933084b726e648824
Anitamalina <anit@itu.dk>
2022-01-02 23:40:19 +0100

    deployment settings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 // Styles
 import "./App.css";
