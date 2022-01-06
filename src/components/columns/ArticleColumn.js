1ebe3f1b7af34cff51bb5aa51710e7d188b1bd3a
Anitamalina <anit@itu.dk>
2022-01-06 13:48:21 +0100

    something works!

--- src/components/columns/ArticleColumn.js
+++ src/components/columns/ArticleColumn.js
@@ -5,6 +5,9 @@ import Loui_Avatar from "./Loui_Avatar.png";
 // Styles
 import "./ArticleColumn.css";
 
+// Function
+import { readJobList } from "../../database/articleRole";
+
 // Components
 import Statusbar from "../statusbar/Statusbar";
 import Label from "../label/Label";
@@ -15,6 +18,8 @@ function ArticleColumn(props) {
     getArticleCard();
   }, []);
 
+  let articleAndJob = new Map()
+  let jobList = [];
 
   function handleClickIdeaPopup(card) {
     /*     console.log("article card clicked!"); */
@@ -23,28 +28,51 @@ function ArticleColumn(props) {
         props.setArticleCardObject(card);
       }
 
+
   async function getArticleCard() {
     const articleObjects = Parse.Object.extend("Article");
     const query = new Parse.Query(articleObjects);
     query.include("workload");
+    query.include("ArticleRole");
     query.equalTo("deadline", props.date.toString().substring(4, 15));
     query.notEqualTo("status", "onhold");
 
     try {
       const articles = await query.find();
-/*       console.log("Parse Objects: ", articles); */
-      const destructuredIdeas = destructureIdeas(articles);
-      props.setColumn(destructuredIdeas);
+
+      let articleIdArr = articles.map((article) => article.id);
+
+      for (let i = 0; i < articleIdArr.length; i++) {
+        let articleId = articleIdArr[i];
+        articleAndJob = articleAndJob.set(articleId, await readJobList(articleId))
+      }
+
+
+      const destructuredArticles = destructureArticles(articles);
+      props.setColumn(destructuredArticles);
 /*       console.log("from readIdeas: ", props.column); */
       return true;
     } catch (error) {
-      alert(`getIdeaCard Error Message ${error.message}`);
+      alert(`getArticleCard Error Message ${error.message}`);
       return false;
     }
   }
 
   function destructure(article) {
-    return {
+
+    getAssignedEmployees(article.id);
+
+    let usernamesFromJobList = jobList.map((job) => job.attributes.user.attributes.username) 
+    let usernamesFromJobListArr = []
+    usernamesFromJobListArr = usernamesFromJobList.map(username => username)
+/*     console.log("usernamesFromJobListArr", usernamesFromJobListArr) */
+
+    let userimagesFromJobList = jobList.map((job) => job.attributes.user.attributes.userimage)
+    let userimagesFromJobListArr = []
+    userimagesFromJobListArr = userimagesFromJobList.map(userimage => userimage.url())
+    console.log("userimagesFromJobListArr", userimagesFromJobListArr)
+
+    let articleObject = {
       id: article.id,
       title: article.get("title"),
       description: article.get("description"),
@@ -54,11 +82,21 @@ function ArticleColumn(props) {
       status: article.get("status"),
       ideaId: article.get("ideaId"),
       ideaAuthor: article.get("ideaAuthor"),
-    };
+      usernames: usernamesFromJobListArr,
+      userimages: userimagesFromJobListArr
+    }
+
+    return articleObject
   }
 
-  function destructureIdeas(article) {
+  function destructureArticles(article) {
     return article.map(destructure);
+  }
+
+  async function getAssignedEmployees(id) {
+    jobList = articleAndJob.get(id)
+/*     console.log("jobList", jobList); */
+    return jobList
   }
 
   return (
@@ -79,7 +117,9 @@ function ArticleColumn(props) {
               <Label sectionName={card.section} />
             </div>
             <div className="assigned-people">
-              <img src={Loui_Avatar} alt="test" />
+              <p>{card.usernames}</p>
+              <img src={card.userimages} alt="userimage"></img>
+              {/* <img src={card.userimage.url()} alt="test" /> */}
             </div>
             <div className="statusbar">
             <Statusbar status={card.status} />
