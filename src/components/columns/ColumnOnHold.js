2d9c6a0fe4a175c96f0200b5ee19f6864f4d32de
Anitamalina <anit@itu.dk>
2022-01-06 14:44:18 +0100

    ColumnOnHold updated with roles

--- src/components/columns/ColumnOnHold.js
+++ src/components/columns/ColumnOnHold.js
@@ -5,56 +5,89 @@ import Loui_Avatar from "./Loui_Avatar.png";
 // Styles
 import "./ArticleColumn.css";
 
+// Function
+import { readJobList } from "../../database/articleRole";
+
 // Components
 import Statusbar from "../statusbar/Statusbar";
 import Label from "../label/Label";
 
 function ColumnOnHold(props) {
-  function handleClickIdeaPopup(card) {
-/*     console.log("article card clicked!"); */
-    props.setPopupArticle(true);
-/*     console.log("is this the id of the card?", card.id); */
-    props.setArticleId(card.id);
-    props.setArticleCardObject(card);
-/*     console.log("this is the card: ", card); */
-  }
 
   useEffect(() => {
     getArticleCard();
   }, []);
 
+  let articleAndJob = new Map()
+  let jobList = [];
+
+  function handleClickIdeaPopup(card) {
+    props.setPopupArticle(true);
+    props.setArticleId(card.id);
+    props.setArticleCardObject(card);
+  }
+
   async function getArticleCard() {
     const articleObjects = Parse.Object.extend("Article");
     const query = new Parse.Query(articleObjects);
     query.include("workload");
+    query.include("ArticleRole");
     query.equalTo("status", "onhold");
 
     try {
       const articles = await query.find();
-/*       console.log("Parse Objects: ", articles); */
-      const destructuredIdeas = destructureIdeas(articles);
-      props.setColumnOnHold(destructuredIdeas);
-/*       console.log("from readIdeas: ", props.columnOnHold); */
+      let articleIdArr = articles.map((article) => article.id);
+
+      for (let i = 0; i < articleIdArr.length; i++) {
+        let articleId = articleIdArr[i];
+        articleAndJob = articleAndJob.set(articleId, await readJobList(articleId))
+      }
+
+      const destructuredArticles = destructureArticles(articles);
+      props.setColumnOnHold(destructuredArticles);
+/*       console.log("from readArticles: ", props.column); */
       return true;
     } catch (error) {
-      alert(`getIdeaCard Error Message ${error.message}`);
+      alert(`getArticleCard Error Message ${error.message}`);
       return false;
     }
   }
 
   function destructure(article) {
-    return {
+    getAssignedEmployees(article.id);
+
+    let usernamesFromJobList = jobList.map((job) => job.attributes.user.attributes.username) 
+    let usernamesFromJobListArr = []
+    usernamesFromJobListArr = usernamesFromJobList.map(username => username)
+
+    let userimagesFromJobList = jobList.map((job) => job.attributes.user.attributes.userimage)
+    let userimagesFromJobListArr = []
+    userimagesFromJobListArr = userimagesFromJobList.map(userimage => userimage.url())
+
+    let articleCardObject = {
       id: article.id,
       title: article.get("title"),
       description: article.get("description"),
       section: article.get("section"),
       deadline: article.get("deadline"),
+      length: article.get("length"),
       status: article.get("status"),
-    };
+      ideaId: article.get("ideaId"),
+      ideaAuthor: article.get("ideaAuthor"),
+      usernames: usernamesFromJobListArr,
+      userimages: userimagesFromJobListArr
+    }
+
+    return articleCardObject
   }
 
-  function destructureIdeas(article) {
+  function destructureArticles(article) {
     return article.map(destructure);
+  }
+
+  function getAssignedEmployees(id) {
+    jobList = articleAndJob.get(id)
+    return jobList
   }
 
   return (
@@ -70,11 +103,12 @@ function ColumnOnHold(props) {
             </div>
             <p>{card.description}</p>
             <div className="tags">
-              {/* <InputTag/> */}
               <Label sectionName={card.section} />
             </div>
             <div className="assigned-people">
-              <img src={Loui_Avatar} alt="test" />
+              {card.userimages.map((userimage) => (
+                <img src={userimage} alt="userimage"></img>
+              ))}
             </div>
             <div className="statusbar">
             <Statusbar status={card.status} />
